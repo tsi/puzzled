d5bdc9edca93e15ee5139c8a22d5bdc76c45f809
tsi <tsi.shlidor@gmail.com>
2014-03-21 00:20:12 +0200

    App

--- js/puzzled-app.js
+++ js/puzzled-app.js
@@ -13,7 +13,14 @@
           imgPath: getParameterByName('img') || 'http://lorempixel.com/400/400/',
           layoutX: getParameterByName('x') || 5,
           layoutY: getParameterByName('y') || 5
-        };
+        },
+        typewatch = (function(){
+          var timer = 0;
+          return function(callback, ms){
+            clearTimeout (timer);
+            timer = setTimeout(callback, ms);
+          }
+        })();
 
     function updateGameSrc() {
       $('iframe').fadeTo('fast', 0, function() {
@@ -44,12 +51,17 @@
 
     setGameDefaults()
 
-    $('form input#img').keydown(function() {
-      updateGameSrc();
+    $('form input#img').keyup(function () {
+      typewatch(function () {
+        // executed only 500 ms after the last keyup event.
+        updateGameSrc();
+      }, 500);
     });
 
-    $('form input[type="number"]').change(function() {
-      updateGameSrc();
+    $('form input[type="number"]').change(function () {
+      typewatch(function () {
+        updateGameSrc();
+      }, 500);
     });
 
     updateGameSrc();
@@ -62,24 +74,24 @@
 
 })(jQuery);
 
-// // Async Sharing Buttons (Facebook, Twitter)
-// // http://css-tricks.com/snippets/javascript/async-sharing-buttons-g-facebook-twitter/
-// (function(doc, script) {
-//   var js,
-//       fjs = doc.getElementsByTagName(script)[0],
-//       frag = doc.createDocumentFragment(),
-//       add = function(url, id) {
-//           if (doc.getElementById(id)) {return;}
-//           js = doc.createElement(script);
-//           js.src = url;
-//           id && (js.id = id);
-//           frag.appendChild( js );
-//       };
+// Async Sharing Buttons (Facebook, Twitter)
+// http://css-tricks.com/snippets/javascript/async-sharing-buttons-g-facebook-twitter/
+(function(doc, script) {
+  var js,
+      fjs = doc.getElementsByTagName(script)[0],
+      frag = doc.createDocumentFragment(),
+      add = function(url, id) {
+          if (doc.getElementById(id)) {return;}
+          js = doc.createElement(script);
+          js.src = url;
+          id && (js.id = id);
+          frag.appendChild( js );
+      };
 
-//     // Facebook SDK
-//     add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
-//     // Twitter SDK
-//     add('//platform.twitter.com/widgets.js');
+    // Facebook SDK
+    add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=200103733347528', 'facebook-jssdk');
+    // Twitter SDK
+    add('//platform.twitter.com/widgets.js');
 
-//     fjs.parentNode.insertBefore(frag, fjs);
-// }(document, 'script'));
+    fjs.parentNode.insertBefore(frag, fjs);
+}(document, 'script'));
